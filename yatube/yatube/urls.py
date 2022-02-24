a05f117d447bd8a1d543dbe597430ef92f3e4ad7
Василий Кузьмин <vasyakuzmin1@yandex.ru>
2022-02-24 04:18:09 +0300

    Кастомные шаблоны ошибок

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -9,3 +9,5 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
 ]
 handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+handler403 = 'core.views.permission_denied'
